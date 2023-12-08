2cadb37b9bb1668a24889e4d8e2978348df9f6d9
Riggs Lab <njg4ne@Nicholass-iMac.local>
2023-12-08 12:30:03 -0500

    fixing tag changing

--- src/components/EditHighlight.jsx
+++ src/components/EditHighlight.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import { highlights, tags } from "../signals/Filesystems";
-import { TagChooser } from "./TagsFilter";
+import { ManagedTagChooser, TagChooser } from "./TagsFilter";
 import { multiWriteQuery } from "../utils/sql";
 
 import List from "@mui/material/List";
@@ -16,117 +16,160 @@ import IconButton from "@mui/material/IconButton";
 import Card from "@mui/material/Card";
 import Button from "@mui/material/Button";
 
-function ChangeExistingTags({ options, onCheck }) {
-    const [checked, setChecked] = React.useState([0]);
+import { signal } from "@preact/signals-react";
 
-    return <List
-        component={Card}
-        sx={{ width: "75%" }}
-    >
-        {options.map(([tid, tagName]) => {
-            const labelId = `checkbox-list-label-${tid}`;
+import LexicalEditor from "./LexicalEditor";
+import { Box } from "@mui/material";
 
-            return (
-                <ListItem
-                    key={tagName}
-                    // secondaryAction={
-                    //     <IconButton edge="end" aria-label="comments">
-                    //         <CommentIcon />
-                    //     </IconButton>
-                    // }
-                    disablePadding
-                >
-                    <ListItemButton
-                        role={undefined}
-                        onClick={(e) => onCheck(e.target.checked, tid)}
-                        dense
-                    >
-                        <ListItemIcon>
-                            <Checkbox
-                                edge="start"
-                                // checked={checked.indexOf(value) !== -1}
-                                tabIndex={-1}
-                                disableRipple
-                                inputProps={{ "aria-labelledby": labelId }}
-                                defaultChecked
-                            />
-                        </ListItemIcon>
-                        <ListItemText id={labelId} primary={`${tagName}`} />
-                    </ListItemButton>
-                </ListItem>
-            );
-        })}
+function ChangeExistingTags({ options, onCheck, disabled }) {
+  const [checked, setChecked] = React.useState([0]);
+
+  return (
+    <List component={Card} sx={{ width: "75%" }}>
+      {options.map(([tid, tagName]) => {
+        const labelId = `checkbox-list-label-${tid}`;
+
+        return (
+          <ListItem
+            key={tagName}
+            // secondaryAction={
+            //     <IconButton edge="end" aria-label="comments">
+            //         <CommentIcon />
+            //     </IconButton>
+            // }
+            disablePadding
+          >
+            <ListItemButton
+              role={undefined}
+              onClick={(e) => onCheck(e.target.checked, tid)}
+              dense
+            >
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  // checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ "aria-labelledby": labelId }}
+                  defaultChecked
+                  disabled={disabled}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`${tagName}`} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
     </List>
+  );
 }
-
+const newTags = signal([]);
 export default function EditHighlight() {
-    const { id } = useParams();
-    const highlight = highlights.value?.find(h => h.id === Number(id));
-    const snippet = highlight?.snippet;
-    let tids = highlight?.tags;
-    const tagEntries = Object.entries(tags.value).filter(([tid, _]) => !tids.includes(Number(tid)));
-    tids = tids || [];
-    tids = tids.map(tid => [`${tid}`, tags.value[tid]]);
-    const hlTags = tids.map(tid => tags[tid]);
+  const { id } = useParams();
+  const highlight = highlights.value?.find((h) => h.id === Number(id));
+  const snippet = highlight?.snippet;
+  let tids = highlight?.tags;
+  const tagEntries = Object.entries(tags.value).filter(
+    ([tid, _]) => !tids.includes(Number(tid))
+  );
+  tids = tids || [];
+  tids = tids.map((tid) => [`${tid}`, tags.value[tid]]);
+  const hlTags = tids.map((tid) => tags[tid]);
+  const [loading, setLoading] = React.useState(false);
+  const [richText, setRichText] = React.useState(null);
 
-    // console.log("tids", tids);
-    const [toRemove, setToRemove] = React.useState(new Set());
-    const [toAdd, setToAdd] = React.useState(new Set());
+  //   const [newTags, setNewTags] = React.useState([]);
 
-    const onChange = (event, value) => {
-        // console.log("onChange", value);
-        // setToAdd((s) => {
-        //     value.map(v => Number(v[0])).forEach((v) => s.add(v));
-        //     return new Set(s);
-        // });
-        setToAdd(new Set(value.map(v => Number(v[0]))));
-    };
-    const onCheck = (checked, id) => {
-        // console.log("onCheck", checked, id)
-        setToRemove((s) => {
-            if (checked) {
-                s.delete(id);
-            }
-            else {
-                s.add(id);
-            }
-            return new Set(s);
-        });
-    };
+  // console.log("tids", tids);
+  const [toRemove, setToRemove] = React.useState(new Set());
+  const [toAdd, setToAdd] = React.useState(new Set());
 
-    // use effect to both sets and console log them
-    // React.useEffect(() => {
-    //     console.log("toRemove", toRemove);
-    //     console.log("toAdd", toAdd);
-    // }, [toRemove, toAdd]);
+  const onChange = (event, value) => {
+    // console.log("onChange", value);
+    // setToAdd((s) => {
+    //     value.map(v => Number(v[0])).forEach((v) => s.add(v));
+    //     return new Set(s);
+    // });
+    console.log("onChange", value);
+    newTags.value = value;
+    setToAdd(new Set(value.map((v) => Number(v[0]))));
+  };
+  const onCheck = (checked, id) => {
+    // console.log("onCheck", checked, id)
+    setToRemove((s) => {
+      if (checked) {
+        s.delete(id);
+      } else {
+        s.add(id);
+      }
+      return new Set(s);
+    });
+  };
 
+  // use effect to both sets and console log them
+  // React.useEffect(() => {
+  //     console.log("toRemove", toRemove);
+  //     console.log("toAdd", toAdd);
+  // }, [toRemove, toAdd]);
 
-    const buttonDisabled = toRemove.size === 0 && toAdd.size === 0;
-    function save() {
-        const highlight_id = Number(id);
-        const tags_to_remove = [...toRemove];
-        const tags_to_add = [...toAdd];
-        let q = "";
-        if (tags_to_remove.length > 0) {
-            q += `DELETE FROM highlight_tags WHERE highlight_id=${highlight_id} AND tag_id IN (${tags_to_remove.join(", ")});`;
-        }
-        if (tags_to_add.length > 0) {
-            q += `INSERT INTO highlight_tags (highlight_id, tag_id) VALUES ${tags_to_add.map(tid => `(${highlight_id}, ${tid})`).join(", ")};`;
-        }
-        // console.log(q);
-        multiWriteQuery(q).then(() => {
-            // TODO update the page controls
-        });
-
+  const buttonDisabled = toRemove.size === 0 && toAdd.size === 0;
+  function save() {
+    const highlight_id = Number(id);
+    const tags_to_remove = [...toRemove];
+    const tags_to_add = [...toAdd];
+    // console.log(
+    //   `save ${highlight_id} remove ${tags_to_remove} add ${tags_to_add}`
+    // );
+    let q = "";
+    if (tags_to_remove.length > 0) {
+      q += `DELETE FROM highlight_tags WHERE highlight_id=${highlight_id} AND tag_id IN (${tags_to_remove.join(
+        ", "
+      )});`;
     }
+    if (tags_to_add.length > 0) {
+      q += `INSERT INTO highlight_tags (highlight_id, tag_id) VALUES ${tags_to_add
+        .map((tid) => `(${highlight_id}, ${tid})`)
+        .join(", ")};`;
+    }
+    console.log(q);
+    newTags.value = [];
+    setLoading(true);
+    multiWriteQuery(q).then(() => {
+      setToAdd(new Set());
+      setToRemove(new Set());
+      setLoading(false);
+      // TODO update the page controls
+    });
+  }
+  //   Button__root___1gz0c define background: yellow for this class
 
-    return <Stack alignItems="center" spacing={2} sx={{ pt: 5 }}>
-        <Typography variant="h4"> Edit Highlight {id}</Typography>
-        <Typography dangerouslySetInnerHTML={{ __html: snippet }} />
-        <Typography variant="h5"> Existing Tags</Typography>
-        <ChangeExistingTags options={tids} onCheck={onCheck} />
-        <Typography variant="h5"> New Tags</Typography>
-        <TagChooser options={tagEntries} sx={{ width: "75%" }} defaultValue={[]} onChange={onChange} />
-        <Button onClick={save} disabled={buttonDisabled} variant="contained">Save</Button>
-    </Stack >;
+  return (
+    <Stack alignItems="center" spacing={2} sx={{ pt: 5 }}>
+      <Typography variant="h4"> Edit Highlight {id}</Typography>
+      <Typography dangerouslySetInnerHTML={{ __html: snippet }} />
+      {/* <Box sx={{ height: "50vh", width: "75%" }}>
+        <LexicalEditor />
+      </Box> */}
+      <Typography variant="h5"> Existing Tags</Typography>
+      <ChangeExistingTags options={tids} onCheck={onCheck} disabled={loading} />
+      <Typography variant="h5"> New Tags</Typography>
+      {/* <TagChooser
+        options={tagEntries}
+        sx={{ width: "75%" }}
+        defaultValue={[]}
+        onChange={onChange}
+      /> */}
+      <ManagedTagChooser
+        options={tagEntries}
+        sx={{ width: "75%" }}
+        defaultValue={[]}
+        onChange={onChange}
+        value={newTags.value}
+        disabled={loading}
+      />
+      <Button onClick={save} disabled={buttonDisabled} variant="contained">
+        Save
+      </Button>
+    </Stack>
+  );
 }
